0bc403ede85c94651ca8030d23f0ade589833531
Sergio Yepes <sergioyepes21@hotmail.com>
2024-04-08 20:19:29 -0500

    adding dependencies and configuration files

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+
+import * as axios from "axios";
+
+export async function handler(event: Record<string, string>) {
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ event, axios: axios.VERSION }),
+  };
+}
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const slsw = require("serverless-webpack");
+const nodeExternals = require("webpack-node-externals");
+
+const isLocal = slsw.lib.webpack.isLocal;
+
+const exportMode = isLocal ? "development" : "production";
+
+const exportExternals = isLocal ? [] : [nodeExternals()];
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: "node",
+  mode: exportMode,
+  externals: exportExternals,
+  optimization: {
+    minimize: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+        exclude: /node_modules/,
+      }
+    ]
+  }
+};
