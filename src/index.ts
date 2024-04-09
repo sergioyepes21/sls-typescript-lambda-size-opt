54120391406febc963c419f66cabb8b5c8579a22
Sergio Yepes <sergioyepes21@hotmail.com>
2024-04-09 09:35:51 -0500

    Removing serverless-plugin-layer-manager dependency since it is not necessary

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-
 import * as axios from "axios";
 
 export async function handler(event: Record<string, string>) {
-
+  
   return {
     statusCode: 200,
     body: JSON.stringify({ event, axios: axios.VERSION }),
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts?$/,
+        test: /\.ts$/,
         loader: "ts-loader",
         options: {
           transpileOnly: true,
